6201ba98b19e90223ef0d7fd76b72a9de955266a
Sergio-Al <sergio.alejandro.ml900@gmail.com>
2025-11-17 10:01:30 -0400

    Checkpoint from VS Code for coding agent session

---
+++ lib/data/repositories/almacen_repository_impl.dart
@@ -0,0 +1,98 @@
+
+
+import 'package:dartz/dartz.dart';
+import 'package:flutter_management_system/core/errors/failures.dart';
+import 'package:flutter_management_system/core/network/network_info.dart';
+import 'package:flutter_management_system/core/sync/sync_manager.dart';
+import 'package:flutter_management_system/data/datasources/local/database/daos/almacen_dao.dart';
+import 'package:flutter_management_system/data/datasources/remote/almacen_remote_datasource.dart';
+import 'package:flutter_management_system/domain/entities/almacen.dart';
+import 'package:flutter_management_system/domain/repositories/almacen_repository.dart';
+
+class AlmacenRepositoryImpl extends AlmacenRepository {
+  final AlmacenRemoteDataSource remoteDataSource;
+  final AlmacenDao almacenDao;
+  final NetworkInfo networkInfo;
+  final SyncManager syncManager;
+
+  AlmacenRepositoryImpl({
+    required this.remoteDataSource,
+    required this.almacenDao,
+    required this.networkInfo,
+    required this.syncManager,
+  });
+
+  @override
+  Future<Either<Failure, Almacen>> createAlmacen(Almacen almacen) {
+    // TODO: implement createAlmacen
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, Unit>> deleteAlmacen(String id) {
+    // TODO: implement deleteAlmacen
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, Almacen>> getAlmacenByCodigo(String codigo) {
+    // TODO: implement getAlmacenByCodigo
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, Almacen>> getAlmacenById(String id) {
+    // TODO: implement getAlmacenById
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, Almacen>> getAlmacenPrincipal(String tiendaId) {
+    // TODO: implement getAlmacenPrincipal
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, List<Almacen>>> getAlmacenes() {
+    // TODO: implement getAlmacenes
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, List<Almacen>>> getAlmacenesActivos() {
+    // TODO: implement getAlmacenesActivos
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, List<Almacen>>> getAlmacenesByTienda(String tiendaId) {
+    // TODO: implement getAlmacenesByTienda
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, List<Almacen>>> getAlmacenesByTipo(String tipo) {
+    // TODO: implement getAlmacenesByTipo
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, List<Almacen>>> searchAlmacenes(String query) {
+    // TODO: implement searchAlmacenes
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, Almacen>> toggleAlmacenActivo(String id) {
+    // TODO: implement toggleAlmacenActivo
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, Almacen>> updateAlmacen(Almacen almacen) {
+    // TODO: implement updateAlmacen
+    throw UnimplementedError();
+  }
+
+  
+}
